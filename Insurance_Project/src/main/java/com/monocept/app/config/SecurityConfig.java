de6687676fe667a34ad1abe9b5f5e9b7b012f1b5
Shivansh-Rastogi094 <rshivansh094@gmail.com>
2026-06-15 19:46:36 +0530

    Fetch issue solved

--- Insurance_Project/src/main/java/com/monocept/app/config/SecurityConfig.java
+++ Insurance_Project/src/main/java/com/monocept/app/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.monocept.app.config;
 
+
+import java.util.List;
+
+import org.springframework.security.config.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.monocept.app.security.JwtAuthenticationFilter;
 
@@ -33,6 +40,7 @@ public class SecurityConfig {
 
 		return http.csrf(csrf -> csrf.disable())
 
+				.cors(Customizer.withDefaults())
 				.authorizeHttpRequests(auth -> auth
 
 						.requestMatchers("/api/auth/**").permitAll()
@@ -65,5 +73,30 @@ public class SecurityConfig {
 
 		return config.getAuthenticationManager();
 	}
+	
+	 @Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	    	System.out.println("CORS CONFIG LOADED");
+
+	        CorsConfiguration configuration = new CorsConfiguration();
+
+	        configuration.setAllowedOrigins(
+	                List.of("http://localhost:5173"));
+
+	        configuration.setAllowedMethods(
+	                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+	        configuration.setAllowedHeaders(
+	                List.of("*"));
+
+	        configuration.setAllowCredentials(true);
+
+	        UrlBasedCorsConfigurationSource source =
+	                new UrlBasedCorsConfigurationSource();
+
+	        source.registerCorsConfiguration("/**", configuration);
+
+	        return source;
+	    }
 
 }
